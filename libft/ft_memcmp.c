7d61553a1f25e57d02f2969d49e205df05b98689
BARETTE <zyrtec06200@live.fr>
2022-06-08 12:59:21 +0200

    Ajout de la libft

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarette <jbarette@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/15 13:15:43 by jbarette          #+#    #+#             */
+/*   Updated: 2021/05/15 13:15:44 by jbarette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	int		*diff;
+
+	i = 0;
+	diff = (int *)&i;
+	while (i < n)
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+		{
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		}
+		i++;
+	}
+	return (0);
+}
