186211da3c910c3355c1df28c54d2b7c4accbae4
Hamza Terras <hterras@c2r1p3.42nice.fr>
2022-06-22 13:54:58 +0200

    plus de leaks

--- srcs/ft_unset.c
+++ srcs/ft_unset.c
@@ -8,31 +8,12 @@ static void delete_element(t_element *element, t_env *env, char *arg)
 		if (ft_strncmp(arg, element->value, ft_strlen(arg)) == 0)
 		{
 			current->next = element->next;
+			free(element->value);
 			free(element);
 			break ;
 		}
 		current = element;
 	}
-    /*t_element *current;
-	t_element *precent;
-
-    current = element;
-	precent = NULL;
-    if (element == NULL)
-        return NULL;
-    if (ft_strncmp(arg, current->value, ft_strlen(arg)) == 0)
-    {
-        precent = current->next;
-		if (current == env->first)
-			env->first = precent;
-        free(current);
-        return precent;
-    }
-	else
-    {
-        element->next = delete_element(element->next, env, arg);
-        return element;
-    }*/
 }
 
 void	ft_unset(t_env *env, char *arg)
