126bd3075123a2aab78159b64f3f252628cf7f05
Hamza Terras <hterras@c1r7p1.42nice.fr>
2022-06-23 17:09:28 +0200

    parsing finish

--- srcs/right_redir.c
+++ srcs/right_redir.c
@@ -6,30 +6,31 @@
 /*   By: hterras <hterras@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 15:42:02 by hterras           #+#    #+#             */
-/*   Updated: 2022/06/23 15:42:56 by hterras          ###   ########.fr       */
+/*   Updated: 2022/06/23 17:08:42 by hterras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int handler_dright_redir(t_parser *parser)
+int	handler_dright_redir(t_parser *parser)
 {
 	int		saveout1;
-	
+
 	saveout1 = dup(1);
 	close(1);
 	dup2(open(parser->parser_heredoc, O_CREAT | O_RDWR | O_APPEND, 0666), 1);
 	return (saveout1);
 }
 
-int handler_right_redir(t_parser *parser)
+int	handler_right_redir(t_parser *parser)
 {
 	int		saveout1;
 	int		fd;
+
 	fd = open(parser->parser_heredoc, O_RDWR);
 	saveout1 = dup(1);
 	close(1);
-	if(fd == -1)
+	if (fd == -1)
 		dup2(open(parser->parser_heredoc, O_CREAT | O_RDWR, 0666), 1);
 	else
 		dup2(open(parser->parser_heredoc, O_TRUNC | O_RDWR, 0666), 1);
