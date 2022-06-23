3daf0714793968d825f48084cb80ce2f0ea81a67
BARETTE <zyrtec06200@live.fr>
2022-06-23 20:31:28 +0200

    Start resolution du unset

--- srcs/ft_unset.c
+++ srcs/ft_unset.c
@@ -6,29 +6,35 @@
 /*   By: jbarette <jbarette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 16:10:55 by jbarette          #+#    #+#             */
-/*   Updated: 2022/06/23 16:16:41 by jbarette         ###   ########.fr       */
+/*   Updated: 2022/06/23 20:30:40 by jbarette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	delete_element(t_element *element, t_env *env, char *arg)
+static t_element* delete_element(t_element *element, t_env *env, char *arg)
 {
-	t_element	*current;
+    t_element *current, *precent;
 
-	current = element;
-	while (element != NULL)
-	{
-		element = element->next;
-		if (ft_strncmp(arg, element->value, ft_strlen(arg)) == 0)
-		{
-			current->next = element->next;
-			free(element->value);
-			free(element);
-			break ;
-		}
-		current = element;
-	}
+    current = element;
+    if (element == NULL)
+        return NULL;
+	char **split_arg = ft_split(current->value, '=');
+    if (ft_strncmp(arg, split_arg[0], ft_strlen(split_arg[0])) == 0)
+    {
+        precent = current->next;
+		if (current == env->first)
+			env->first = precent;
+		free(current->value);
+        free(current);
+        return precent;
+    }
+	else
+    {
+        element->next = delete_element(element->next, env, arg);
+        return element;
+    }
+	free_array(split_arg);
 }
 
 void	ft_unset(t_env *env, char *arg)
