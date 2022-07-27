18a38fbea7d02847b7c22275621f2fc77411f1d6
nothxmza <you@example.com>
2022-07-27 10:23:43 +0200

    norm

--- srcs/init.c
+++ srcs/init.c
@@ -20,15 +20,8 @@ t_parser	*init(void)
 	parser->parser_cmd = NULL;
 	parser->parser_opt = 0;
 	parser->parser_args = NULL;
-	parser->parser_double_quote = 0;
-	parser->parser_single_quote = 0;
-	parser->parser_left_redir = 0;
-	parser->parser_right_redir = 0;
-	parser->parser_dleft_redir = 0;
-	parser->parser_dright_redir = 0;
 	parser->parser_sig = 0;
 	parser->parser_count = 0;
-	parser->parser_heredoc = NULL;
 	parser->parser_pwd = NULL;
 	parser->parser_arguments = NULL;
 	return (parser);
@@ -53,7 +46,6 @@ void	free_parser(t_parser *parser)
 {
 	free(parser->parser_cmd);
 	free(parser->parser_args);
-	free(parser->parser_heredoc);
 	free(parser->parser_arguments);
 	free(parser);
 }
