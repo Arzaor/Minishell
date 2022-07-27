cb88cccab13516d2c1dd9f38317e7faa17fce1a8
jeremybarette <jeremy.barette@outlook.fr>
2022-07-27 16:59:57 +0200

    Norm

--- srcs/parsing_args_save.c
+++ srcs/parsing_args_save.c
@@ -6,90 +6,34 @@
 /*   By: jbarette <jbarette@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 17:51:06 by hterras           #+#    #+#             */
-/*   Updated: 2022/07/27 16:12:23 by jbarette         ###   ########.fr       */
+/*   Updated: 2022/07/27 16:57:50 by jbarette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	fsq2(t_parser *parser, t_env *env, char quote, int i)
+static int	transform_arg_save1(t_parser *parser, int i)
 {
 	int	k;
 
 	k = 0;
-	while (parser->parser_args[i] != quote)
+	if (g_code != 0)
 	{
-		if (parser->parser_args[i] == '$' && parser->parser_args[i + 1] != '?')
-			i = get_var_env(parser, i, env, 0);
-		else if (parser->parser_args[i] == '$' && \
-					parser->parser_args[i + 1] == '?')
-		{
-			if (g_code != 0)
-			{
-				while (ft_itoa_base(g_code, 10)[k])
-					parser->parser_arguments[parser->parser_count++] = \
-							ft_itoa_base(g_code, 10)[k++];
-			}
-			else
-				parser->parser_arguments[parser->parser_count++] = '0';
-			i += 2;
-		}
-		else
+		while (ft_itoa_base(g_code, 10)[k])
 			parser->parser_arguments[parser->parser_count++] = \
-					parser->parser_args[i++];
-	}
-	return (i);
-}
-
-static int	fsq3(t_parser *parser, t_env *env, char quote, int i)
-{
-	if (quote == '\'')
-	{
-		while (parser->parser_args[i] != quote)
-			parser->parser_arguments[parser->parser_count++] = \
-				parser->parser_args[i++];
+				ft_itoa_base(g_code, 10)[k++];
 	}
 	else
-		i = fsq2(parser, env, quote, i);
-	return (i);
-}
-
-static int	found_second_quote_save(t_parser *parser, int i, \
-				char quote, t_env *env)
-{
-	int	result;
-	int	tmp;
-
-	result = 0;
-	if (parser->parser_args[i + 1])
-		i += 1;
-	else
-		return (-1);
-	tmp = i;
-	while (parser->parser_args[i])
-	{
-		if (parser->parser_args[i] == quote)
-		{
-			result = 1;
-			break ;
-		}
-		i++;
-	}
-	i = tmp;
-	if (result)
-		i = fsq3(parser, env, quote, i);
-	else
-		return (-1);
+		parser->parser_arguments[parser->parser_count++] = '0';
+	i += 2;
 	return (i);
 }
 
 static void	transform_arg_save(t_parser *parser, t_env *env)
 {
 	int	i;
-	int	k;
 
 	i = 0;
-	k = 0;
 	while (parser->parser_args[i])
 	{
 		if (parser->parser_args[i] != '\'' && parser->parser_args[i] \
@@ -109,17 +53,7 @@ static void	transform_arg_save(t_parser *parser, t_env *env)
 			i = get_var_env(parser, i, env, 0);
 		else if (parser->parser_args[i] == '$' && \
 				parser->parser_args[i + 1] == '?')
-		{
-			if (g_code != 0)
-			{
-				while (ft_itoa_base(g_code, 10)[k])
-					parser->parser_arguments[parser->parser_count++] = \
-						ft_itoa_base(g_code, 10)[k++];
-			}
-			else
-				parser->parser_arguments[parser->parser_count++] = '0';
-			i += 2;
-		}
+			i = transform_arg_save1(parser, i);
 	}
 	parser->parser_arguments[parser->parser_count] = '\0';
 }
